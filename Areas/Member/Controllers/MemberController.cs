580aaf81d707fec48c0869bff39c9b3fc4225e5f
JShantz2022 <josslyneshantz@gmail.com>
2023-10-10 16:10:07 -0400

    Revert "Revert "Added profile page""
    This reverts commit c8b9689f3beba76d4762e5c317a2244e7deb554c.

---
+++ Areas/Member/Controllers/MemberController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG3050_HMJJ.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class MemberController : Controller
+    {
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
