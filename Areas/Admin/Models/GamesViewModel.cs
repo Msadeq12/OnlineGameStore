96f8076c517b582d462f8ac8766eadc7a275fa86
Mohammad Sadeq Khandakar <msadeq27@yahoo.com>
2023-11-02 17:35:06 -0400

    Carousel Error
    MSK - Added selecting-games feature to Member Area. Issues with the carousel.

---
+++ Areas/Admin/Models/GamesViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace PROG3050_HMJJ.Areas.Admin.Models
+{
+    public class GamesViewModel
+    {
+        public int ID { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public int? ReleaseYear { get; set; }
+
+        public string? GameGenre { get; set; }
+
+        public string? GamePlatform { get; set; }
+    }
+}
