51630094f33dd308b6bf7da9c83185d07037d21c
JShantz2022 <josslyneshantz@gmail.com>
2023-11-02 17:27:58 -0400

    Updated Address class validation
    Made most fields required and added a check in the custom validation to see if the country is null before getting the ID.
    
    JS

--- Areas/Member/Models/Address.cs
+++ Areas/Member/Models/Address.cs
@@ -11,57 +11,75 @@ namespace PROG3050_HMJJ.Areas.Member.Models
 
         public virtual User? user { get; set; }
 
+        [Required(ErrorMessage = "Address line 1 is required")]
         [Display(Name = "Address line 1")]
-        public string? Line1 { get; set; }
+        public string Line1 { get; set; }
 
         [Display(Name = "Address Line 2")]
         public string? Line2 { get; set; }
 
+        [Required]
         [Display(Name = "City")]
-        public string? City { get; set; }
+        public string City { get; set; }
 
-        public string? PostalCode { get; set; }
+        public string PostalCode { get; set; }
 
-        public virtual Countries? Country { get; set; }
+        [Required]
+        public virtual Countries Country { get; set; }
 
-        public virtual Regions? Region { get; set; }
+        [Required]
+        public virtual Regions Region { get; set; }
+
+        public string? DeliveryInstructions { get; set; }
+
+        public Boolean SameAddress { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (!String.IsNullOrWhiteSpace(PostalCode))
             {
-                Regex regex;
-                if (Country.ID == 1)
+                if (Country != null)
                 {
-                    regex = new Regex("^[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKMNPRSTVWXYZ][0-9]$");
-                    if (!regex.Match(PostalCode).Success)
+                    Regex regex;
+                    if (Country.ID == 1)
                     {
-                        yield return new ValidationResult("A valid Postal Code is required", new List<string>{ "PostalCode" });
+                        regex = new Regex("^[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKMNPRSTVWXYZ][0-9]$");
+                        if (!regex.Match(PostalCode).Success)
+                        {
+                            yield return new ValidationResult("A valid Postal Code is required", new List<string> { "PostalCode" });
+                        }
+                    }
+                    else if (Country.ID == 2)
+                    {
+                        regex = new Regex("^\\d{5}(-{0,1}\\d{4})?$");
+                        if (!regex.Match(PostalCode).Success)
+                        {
+                            yield return new ValidationResult("A valid Zip Code is required", new List<string> { "PostalCode" });
+                        }
                     }
                 }
-                else if (Country.ID == 2)
+                else
                 {
-                    regex = new Regex("^\\d{5}(-{0,1}\\d{4})?$");
-                    if (!regex.Match(PostalCode).Success)
-                    {
-                        yield return new ValidationResult("A valid Zip Code is required", new List<string> { "PostalCode" });
-                    }
+                    yield return new ValidationResult("A country is required", new List<string> { "Country" });
                 }
             }
             else
             {
                 if(Region != null)
                 {
-                    string codeType = "";
-                    if(Country.ID == 1)
+                    if (Country != null)
                     {
-                        codeType = "Postal Code";
+                        string codeType = "";
+                        if (Country.ID == 1)
+                        {
+                            codeType = "Postal Code";
+                        }
+                        else if (Country.ID == 2)
+                        {
+                            codeType = "Zip Code";
+                        }
+                        yield return new ValidationResult(String.Format("A {0} is required", codeType), new List<string> { "PostalCode" });
                     }
-                    else if (Country.ID == 2)
-                    {
-                        codeType = "Zip Code";
-                    }
-                    yield return new ValidationResult(String.Format("A {0} is required", codeType), new List<string> { "PostalCode" });
                 }
             }
         }
