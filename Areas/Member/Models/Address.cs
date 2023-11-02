5e7762584b85c7af930121459c994c26ae6f5709
JShantz2022 <josslyneshantz@gmail.com>
2023-11-02 17:06:39 -0400

    Added models for address
    Created the Address, Countries, and Regions classes/models. Added them to the database context and seeded countries with Canada and USA and Regions with provinces, states, and territories.
    
    JS

---
+++ Areas/Member/Models/Address.cs
@@ -0,0 +1,69 @@
+﻿using PROG3050_HMJJ.Models.Account;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace PROG3050_HMJJ.Areas.Member.Models
+{
+    public class Address : IValidatableObject
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public virtual User? user { get; set; }
+
+        [Display(Name = "Address line 1")]
+        public string? Line1 { get; set; }
+
+        [Display(Name = "Address Line 2")]
+        public string? Line2 { get; set; }
+
+        [Display(Name = "City")]
+        public string? City { get; set; }
+
+        public string? PostalCode { get; set; }
+
+        public virtual Countries? Country { get; set; }
+
+        public virtual Regions? Region { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!String.IsNullOrWhiteSpace(PostalCode))
+            {
+                Regex regex;
+                if (Country.ID == 1)
+                {
+                    regex = new Regex("^[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKMNPRSTVWXYZ][0-9]$");
+                    if (!regex.Match(PostalCode).Success)
+                    {
+                        yield return new ValidationResult("A valid Postal Code is required", new List<string>{ "PostalCode" });
+                    }
+                }
+                else if (Country.ID == 2)
+                {
+                    regex = new Regex("^\\d{5}(-{0,1}\\d{4})?$");
+                    if (!regex.Match(PostalCode).Success)
+                    {
+                        yield return new ValidationResult("A valid Zip Code is required", new List<string> { "PostalCode" });
+                    }
+                }
+            }
+            else
+            {
+                if(Region != null)
+                {
+                    string codeType = "";
+                    if(Country.ID == 1)
+                    {
+                        codeType = "Postal Code";
+                    }
+                    else if (Country.ID == 2)
+                    {
+                        codeType = "Zip Code";
+                    }
+                    yield return new ValidationResult(String.Format("A {0} is required", codeType), new List<string> { "PostalCode" });
+                }
+            }
+        }
+    }
+}
--- Models/DataAccess/GameStoreDbContext.cs
+++ Models/DataAccess/GameStoreDbContext.cs
@@ -47,6 +47,86 @@ namespace PROG3050_HMJJ.Models.DataAccess
                 new Languages { ID = 9, Name = "Japanese" },
                 new Languages { ID = 10, Name = "Italian" }
                 );
+
+            builder.Entity<Countries>().HasData(
+                new Countries { ID = 1, Name = "Canada" },
+                new Countries { ID = 2, Name = "USA"}
+                );
+
+            builder.Entity<Regions>().HasData(
+                new Regions { ID = 1, Name = "Alberta", CountryID = 1 },
+                new Regions { ID = 2, Name = "British Columbia", CountryID = 1 },
+                new Regions { ID = 3, Name = "Manitoba", CountryID = 1 },
+                new Regions { ID = 4, Name = "New Brunswick", CountryID = 1 },
+                new Regions { ID = 5, Name = "Newfoundland and Labrador", CountryID = 1 },
+                new Regions { ID = 6, Name = "Northwest Territories", CountryID = 1 },
+                new Regions { ID = 7, Name = "Nova Scotia", CountryID = 1 },
+                new Regions { ID = 8, Name = "Nunavut", CountryID = 1 },
+                new Regions { ID = 9, Name = "Ontario", CountryID = 1 },
+                new Regions { ID = 10, Name = "Prince Edward Island", CountryID = 1 },
+                new Regions { ID = 11, Name = "Quebec", CountryID = 1 },
+                new Regions { ID = 12, Name = "Saskatchewan", CountryID = 1 },
+                new Regions { ID = 13, Name = "Yukon", CountryID = 1 },
+                new Regions { ID = 14, Name = "Alabama", CountryID = 2 },
+                new Regions { ID = 15, Name = "Alaska", CountryID = 2 },
+                new Regions { ID = 16, Name = "American Samoa", CountryID = 2 },
+                new Regions { ID = 17, Name = "Arizona", CountryID = 2 },
+                new Regions { ID = 18, Name = "Arkansas", CountryID = 2 },
+                new Regions { ID = 19, Name = "California", CountryID = 2 },
+                new Regions { ID = 20, Name = "Colorado", CountryID = 2 },
+                new Regions { ID = 21, Name = "Connecticut", CountryID = 2 },
+                new Regions { ID = 22, Name = "Delaware", CountryID = 2 },
+                new Regions { ID = 23, Name = "Federated States of Micronesia", CountryID = 2 },
+                new Regions { ID = 24, Name = "Florida", CountryID = 2 },
+                new Regions { ID = 25, Name = "Georgia", CountryID = 2 },
+                new Regions { ID = 26, Name = "Guam", CountryID = 2 },
+                new Regions { ID = 27, Name = "Hawaii", CountryID = 2 },
+                new Regions { ID = 28, Name = "Idaho", CountryID = 2 },
+                new Regions { ID = 29, Name = "Illinois", CountryID = 2 },
+                new Regions { ID = 30, Name = "Indiana", CountryID = 2 },
+                new Regions { ID = 31, Name = "Iowa", CountryID = 2 },
+                new Regions { ID = 32, Name = "Kansas", CountryID = 2 },
+                new Regions { ID = 33, Name = "Kentucky", CountryID = 2 },
+                new Regions { ID = 34, Name = "Louisiana", CountryID = 2 },
+                new Regions { ID = 35, Name = "Maine", CountryID = 2 },
+                new Regions { ID = 36, Name = "Marshall Islands", CountryID = 2 },
+                new Regions { ID = 37, Name = "Maryland", CountryID = 2 },
+                new Regions { ID = 38, Name = "Massachusetts", CountryID = 2 },
+                new Regions { ID = 39, Name = "Michigan", CountryID = 2 },
+                new Regions { ID = 40, Name = "Minnesota", CountryID = 2 },
+                new Regions { ID = 41, Name = "Mississippi", CountryID = 2 },
+                new Regions { ID = 42, Name = "Missouri", CountryID = 2 },
+                new Regions { ID = 43, Name = "Montana", CountryID = 2 },
+                new Regions { ID = 44, Name = "Nebraska", CountryID = 2 },
+                new Regions { ID = 45, Name = "Nevada", CountryID = 2 },
+                new Regions { ID = 46, Name = "New Hampshire", CountryID = 2 },
+                new Regions { ID = 47, Name = "New Jersey", CountryID = 2 },
+                new Regions { ID = 48, Name = "New Mexico", CountryID = 2 },
+                new Regions { ID = 49, Name = "New York", CountryID = 2 },
+                new Regions { ID = 50, Name = "North Carolina", CountryID = 2 },
+                new Regions { ID = 51, Name = "North Dakota", CountryID = 2 },
+                new Regions { ID = 52, Name = "Northern Mariana Islands", CountryID = 2 },
+                new Regions { ID = 53, Name = "Ohio", CountryID = 2 },
+                new Regions { ID = 54, Name = "Oklahoma", CountryID = 2 },
+                new Regions { ID = 55, Name = "Oregon", CountryID = 2 },
+                new Regions { ID = 56, Name = "Palau", CountryID = 2 },
+                new Regions { ID = 57, Name = "Pennsylvania", CountryID = 2 },
+                new Regions { ID = 58, Name = "Puerto Rico", CountryID = 2 },
+                new Regions { ID = 59, Name = "Rhode Island", CountryID = 2 },
+                new Regions { ID = 60, Name = "South Carolina", CountryID = 2 },
+                new Regions { ID = 61, Name = "South Dakota", CountryID = 2 },
+                new Regions { ID = 62, Name = "Tennessee", CountryID = 2 },
+                new Regions { ID = 63, Name = "Texas", CountryID = 2 },
+                new Regions { ID = 64, Name = "U.S. Minor Outlying Islands", CountryID = 2 },
+                new Regions { ID = 65, Name = "U.S. Virgin Islands", CountryID = 2 },
+                new Regions { ID = 66, Name = "Utah", CountryID = 2 },
+                new Regions { ID = 67, Name = "Vermont", CountryID = 2 },
+                new Regions { ID = 68, Name = "Virginia", CountryID = 2 },
+                new Regions { ID = 69, Name = "Washington", CountryID = 2 },
+                new Regions { ID = 70, Name = "West Virginia", CountryID = 2 },
+                new Regions { ID = 71, Name = "Wisconsin", CountryID = 2 },
+                new Regions { ID = 72, Name = "Wyoming", CountryID = 2 }
+                );
         }
 
         public static async Task CreateAdminUser(IServiceProvider serviceProvider)
@@ -96,6 +176,15 @@ namespace PROG3050_HMJJ.Models.DataAccess
 
 
         public DbSet<Profiles> Profiles { get; set; }
+
+
+        public DbSet<Address> Address { get; set; }
+
+
+        public DbSet<Countries> Countries { get; set; }
+
+
+        public DbSet<Regions> Regions { get; set; }
 
 
         public GameStoreDbContext(DbContextOptions<GameStoreDbContext> options) : base(options)
