26444c46a45071f222bd715e918b17115264a7c4
Harshal <hthavrani3610@conestogac.on.ca>
2023-11-02 20:50:16 -0400

    Report test HT

---
+++ Areas/Admin/Controllers/ReportController.cs
@@ -0,0 +1,90 @@
+﻿using Aspose.Pdf;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging;
+using PROG3050_HMJJ.Areas.Admin.Models;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace PROG3050_HMJJ.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ReportController : Controller
+    {
+        private readonly Reportdbo _reportdbo;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ReportController(IConfiguration configuration)
+        {
+            _reportdbo = new Reportdbo(configuration);
+        }
+
+        public IActionResult GetMemberDetails()
+        {
+            using (var connection = _reportdbo.GetConnection())
+            {
+                var document = new Document
+                {
+                    PageInfo = new PageInfo { Margin = new MarginInfo(28, 28, 28, 40) }
+                };
+                var pdfpage = document.Pages.Add();
+                Table table = new Table
+                {
+                    ColumnWidths = "20% 20% 20% 20% 20%",
+                    DefaultCellPadding = new MarginInfo(10, 5, 10, 5),
+                    Border = new BorderInfo(BorderSide.All, .5f, Color.Black),
+                    DefaultCellBorder = new BorderInfo(BorderSide.All, .2f, Color.Black)
+                };
+                DataTable dt = _reportdbo.GetMemberDetailsrecord();//GetMemberListsrecord
+                table.ImportDataTable(dt,true,0,0);
+                document.Pages[1].Paragraphs.Add(table);
+                
+                using (var streamout = new MemoryStream())
+                {
+                    document.Save(streamout);
+                    return new FileContentResult(streamout.ToArray(), "application/pdf")
+                    {
+                        FileDownloadName = "CVGS_Member_Detail_Report.pdf",
+                        
+                    };
+                }
+            }
+        }
+
+        public IActionResult GetMemberList()
+        {
+            using (var connection = _reportdbo.GetConnection())
+            {
+                var document = new Document
+                {
+                    PageInfo = new PageInfo { Margin = new MarginInfo(28, 28, 28, 40) }
+                };
+                var pdfpage = document.Pages.Add();
+                Table table = new Table
+                {
+                    ColumnWidths = "10% 40% 40%",
+                    DefaultCellPadding = new MarginInfo(10, 5, 10, 5),
+                    Border = new BorderInfo(BorderSide.All, .5f, Color.Black),
+                    DefaultCellBorder = new BorderInfo(BorderSide.All, .2f, Color.Black)
+                };
+                DataTable dt = _reportdbo.GetMemberListsrecord();
+                table.ImportDataTable(dt, true, 0, 0);
+                document.Pages[1].Paragraphs.Add(table);
+
+                using (var streamout = new MemoryStream())
+                {
+                    document.Save(streamout);
+                    return new FileContentResult(streamout.ToArray(), "application/pdf")
+                    {
+                        FileDownloadName = "CVGS_Member_List_Report.pdf",
+
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ Areas/Admin/Models/Reportdbo.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace PROG3050_HMJJ.Areas.Admin.Models
+{
+    public class Reportdbo
+    {
+        private readonly IConfiguration _configuration;
+
+        public Reportdbo(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            string connectionString = _configuration.GetConnectionString("GameStoreCNN");
+            return new SqlConnection(connectionString);
+        }
+
+        public DataTable GetMemberDetailsrecord()
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                SqlCommand cmd = new SqlCommand("SELECT FirstName,LastName,Gender,DOB,RecievePromotions " +
+                "FROM Profiles " +
+                "WHERE FirstName IS NOT NULL AND LastName IS NOT NULL AND Gender IS NOT NULL AND DOB IS NOT NULL", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt; 
+            }
+        }
+
+        public DataTable GetMemberListsrecord()
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                SqlCommand cmd = new SqlCommand("SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS Sr_No,FirstName,LastName " +
+                "FROM Profiles " +
+                "WHERE FirstName IS NOT NULL AND LastName IS NOT NULL", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}
