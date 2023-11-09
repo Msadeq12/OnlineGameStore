4d4b8f4c29f8765a7300ca12fe00d5ba9e5ddb20
Mohammad Sadeq Khandakar <msadeq27@yahoo.com>
2023-11-08 22:11:27 -0500

    MSK - Added view component folders and views.

---
+++ Areas/Member/Components/GamesByGenre.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore;
+using PROG3050_HMJJ.Areas.Member.Helper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PROG3050_HMJJ.Models.Account;
+using PROG3050_HMJJ.Models.DataAccess;
+
+namespace PROG3050_HMJJ.Areas.Member.Components
+{
+    public class GamesByGenre : ViewComponent
+    {
+        private GameStoreDbContext _context;
+        private UserManager<User> _userManager;
+
+        public GamesByGenre(GameStoreDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            string baseUrl = "$https://localhost:7108/api/game/GameByGenre?";
+
+            return View();
+        }  
+    }
+}
---
+++ Areas/Member/Helper/Helper.cs
@@ -0,0 +1,27 @@
+﻿namespace PROG3050_HMJJ.Areas.Member.Helper
+{
+    public static class Helper
+    {
+        public static string QueryUrlParser(List<string> collection, string url, string endpoint)
+        {
+            
+            for (int i = 0; i <= collection.Count - 1; i++)
+            {
+                url += $"{endpoint}=" + collection[i];
+
+                if (collection.Count > 1)
+                {
+                    url += "&";
+                }
+            }
+
+            if(collection.Count > 1)
+            {
+                url = url.Remove(url.Length - 1);
+            }
+
+           
+            return url;
+        }
+    }
+}
