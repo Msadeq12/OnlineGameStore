42ca85d8ad071702671e63ef3b397a4c05391599
Harshal <hthavrani3610@conestogac.on.ca>
2023-12-01 14:12:28 -0500

    updated the order staus in the cart feature and added the download feature - HT 2023-12-01

--- Areas/Member/Models/CartItem.cs
+++ Areas/Member/Models/CartItem.cs
@@ -9,6 +9,8 @@ namespace PROG3050_HMJJ.Areas.Member.Models
         public string GameName { get; set; }
         public int Price { get; set; }
         public int Quantity { get; set; }
+        public string OrderType { get; set; }  // Added this to indicate the type of order ("Physical" or "Digital")
+        public bool IsPurchased { get; set; }
         public int TotalPrice { get { return Price * Quantity; } }
 
     }
--- Areas/Member/Models/Order.cs
+++ Areas/Member/Models/Order.cs
@@ -12,6 +12,9 @@ namespace PROG3050_HMJJ.Areas.Member.Models
         public int UnitPrice { get; set; }
         public int TotalPrice { get; set; }
         public DateTime OrderDate { get; set; }
+        public string OrderType { get; set; }
+        public bool IsPurchased { get; set; }
+        public string Status { get; set; } // "Processed", "Unprocessed"
         [ForeignKey("Invoice")]
         public int InvoiceId { get; set; } // Foreign key to Invoice
         public Invoice Invoice { get; set; }
